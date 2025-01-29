4581c46f5d05cb2ef90f98a78a7e3d2ce2008149
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-01-28 17:23:28 -0800

    Update count1bits_tests.rkt

--- count1bits_tests.rkt
+++ count1bits_tests.rkt
@@ -3,3 +3,14 @@
 (require rackunit)
 (require "count1bits.rkt")
 (provide count1bits)
+
+;; student tests
+(check-equal? (count1bits 1) 1)
+(check-equal? (count1bits 2) 1)
+(check-equal? (count1bits 3) 2)
+(check-equal? (count1bits 5) 2)
+
+;; provided tests
+(check-equal? (count1bits 6) 2)
+(check-equal? (count1bits 7) 3)
+(check-equal? (count1bits 42) 3)
