ffed4ef814d035e2b0a3799c4f81169c8cb86ae4
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-01-29 13:42:58 -0800

    Update power_tests.rkt

--- power_tests.rkt
+++ power_tests.rkt
@@ -1,1 +1,20 @@
 #lang racket
+
+(require rackunit)
+(require "power.rkt")
+
+;; provided tests
+(check-equal? (power 2 10) 1024)
+(check-equal? (power 42 10) 17080198121677824)
+(check-equal? (fast-power 2 10) 1024)
+(check-equal? (fast-power 42 10) 17080198121677824)
+
+;; student tests
+(check-equal? (power 3 0) 1)
+(check-equal? (power 5 2) 25)
+(check-equal? (power 1 3) 1)
+(check-equal? (power 2 3) 8)
+(check-equal? (fast-power 3 0) 1)
+(check-equal? (fast-power 5 2) 25)
+(check-equal? (fast-power 1 3) 1)
+(check-equal? (fast-power 2 3) 8)
