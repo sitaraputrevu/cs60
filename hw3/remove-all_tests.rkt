1e90452dcbb6a019f83a52fd9dd732a3053d0b32
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-08 15:02:47 -0800

    Update remove-all_tests.rkt

--- hw3/remove-all_tests.rkt
+++ hw3/remove-all_tests.rkt
@@ -5,7 +5,11 @@
 
 
 ; student tests
-
+(check-equal? (remove-all 3 '()) '())
+(check-equal? (remove-all 2 '(1 4 5)) '(1 4 5))
+(check-equal? (remove-all 2 '(1 2 4 5)) '(1 4 5))
+(check-equal? (remove-all 2 '(1 2 (2 4) 5)) '(1 (2 4) 5))
+(check-equal? (remove-all "e" '("e" "a" "i" "e" "e" "o")) '("a" "i" "o"))
 
 ; provided tests
 (check-equal? (remove-all "i" '("a" "l" "i" "i" "i" "e" "n")) 
