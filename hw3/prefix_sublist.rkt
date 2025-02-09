5cf5c831be6599c44a18ae23c915ecdc13cb8e94
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-08 16:08:03 -0800

    Update prefix_sublist.rkt

--- hw3/prefix_sublist.rkt
+++ hw3/prefix_sublist.rkt
@@ -2,12 +2,20 @@
 
 (provide prefix? sublist?)
 
-;; add comment
-(define (sublist? S L)
-
-)
-
-;; add comment
+;; takes in two lists P and L, returns true if P is identical to the initial elements in L
 (define (prefix? P L)
+  (cond
+    [(empty? P) #t] ; base case (empty list is a prefix of any list)
+    [(empty? L) #f] ; if L is empty and P is not, P is not a prefix of L
+    [(= (first P) (first L)) (prefix? (rest P) (rest L))]
+    [#f]
+))
 
-)
+;; takes in two lists S and L, returns true if S is identical to some set of consecutive elements in L 
+(define (sublist? S L)
+  (cond
+    [(empty? S) #t] ; base case (empty list is a sublist of any list)
+    [(empty? L) #f] ; if L is empty and S is not, S is not a sublist of L
+    [(prefix? S L) #t] 
+    [else (sublist? (rest S) (rest L))]
+))
