9c04884a48351a998b7dbf4ed9cc37a5198ef7c0
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-11 10:56:10 -0800

    Update enumerate.rkt

--- hw3/enumerate.rkt
+++ hw3/enumerate.rkt
@@ -3,8 +3,12 @@
 (provide enumerate)
 
 ;; add comment
-(define (findIndex L
+(define (indexList num L)
+  (list num (first L)))
 
-; add comment
+;; add comment
 (define (enumerate L)
+  (if (empty? L)
+  '()
+  (cons (indexList 0 L) (enumerate (rest L))))
 )
