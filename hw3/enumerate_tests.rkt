eee45eabe7033b99c225b0cf30cbdd1edaecc9a5
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-10 17:40:57 -0800

    Update enumerate_tests.rkt

--- hw3/enumerate_tests.rkt
+++ hw3/enumerate_tests.rkt
@@ -1,1 +1,9 @@
 
+(check-equal? (enumerate '(jan feb mar apr)) 
+ '((0 jan) (1 feb) (2 mar) (3 apr)))
+
+(check-equal? (enumerate '(0 I II III IV V VI)) 
+                         '((0 0) (1 I) (2 II) (3 III) (4 IV) (5 V) (6 VI)))
+
+(check-equal? (enumerate '())  '())
+
