358ad329e3aa68233182eb302b1e93d94cde18b7
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-11 17:38:32 -0800

    Update superduper_tests.rkt

--- hw3/superduper_tests.rkt
+++ hw3/superduper_tests.rkt
@@ -2,3 +2,19 @@
 
 (require rackunit)
 (require "superduper.rkt")
+
+; student tests
+
+
+; provided tests
+(check-equal? (superreverse '( (1 2 3) (4 5 6 (7 8) 9 ) ))
+                            '( (3 2 1) (9 (7 8) 6 5 4) ) )
+
+(check-equal? (superreverse '( (1 2 3) (4 5 6) (#\k #\o #\o #\l) (#\a #\m) ))
+                            '( (3 2 1) (6 5 4) (#\l #\o #\o #\k) (#\m #\a) ) )
+
+(check-equal? (duperreverse '( (1 2 3) (4 5 6) 42 ("k" "o" "o" "l") ("a" "m") ))
+                            '( ("m" "a") ("l" "o" "o" "k") 42 (6 5 4) (3 2 1) ))
+
+(check-equal? (duperreverse '( (1 2 3) (4 5 6 (7 8) 9) ))
+                            '( (9 (8 7) 6 5 4) (3 2 1) ))
