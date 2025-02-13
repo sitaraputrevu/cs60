2a52a21af270fbc1a3eedabee1a9c33b8fb2e4fc
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-12 21:55:29 -0800

    Update scrabble.rkt

--- hw3/scrabble.rkt
+++ hw3/scrabble.rkt
@@ -2,6 +2,8 @@
 
 (provide subbag? best-word)
 
+;; takes in two lists S and B, returns true if every element of S is in every element of B (not dependent on order)
+;; the count of any element in S is at least as large in B
 (define (subbag? S B)
   (cond
     [(empty? S) #t]
@@ -9,5 +11,41 @@
     [else #f])
 )
 
-(define (best-word rack WL)
+;; scrabble-tile-bag  
+;;   letter tile scores and counts from the game of Scrabble
+;;   the counts are not needed
+;;   obtained from http://en.wikipedia.org/wiki/Image:Scrabble_tiles_en.jpg
+(define scrabble-tile-bag
+  '((#\a 1 9) (#\b 3 2) (#\c 3 2) (#\d 2 4) (#\e 1 12)
+   (#\f 4 2) (#\g 2 3) (#\h 4 2) (#\i 1 9) (#\j 8 1)
+   (#\k 5 1) (#\l 1 4) (#\m 3 2) (#\n 1 6) (#\o 1 8)
+   (#\p 3 2) (#\q 10 1)(#\r 1 6) (#\s 1 4) (#\t 1 6)
+   (#\u 1 4) (#\v 4 2) (#\w 4 2) (#\x 8 1) (#\y 4 2)
+   (#\z 10 1) (#\_ 0 2)) )
+
+;; comment
+(define (letterScore letter)
+  (second (assoc letter scrabble-tile-bag))
 )
+
+;; comment
+(define (wordScore word)
+  (if (empty? word)
+  0
+  (+ (letterScore (first word))(wordScore (rest word)))
+))
+[let (wordList (string->list word))
+
+(define (highScore WL)
+ (let [highestScore 0]
+  (if (> wordScore (first WL) highestScore)
+    
+)
+))
+
+;; comment
+(define (best-word rack WL)
+  (let [highestScore 0]
+  (if ()
+)
+))
