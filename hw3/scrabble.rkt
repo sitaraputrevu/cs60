56708e6ea25eb60ee7fb74e665d0b654d00e673e
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-12 21:34:43 -0800

    Update scrabble.rkt

--- hw3/scrabble.rkt
+++ hw3/scrabble.rkt
@@ -3,6 +3,10 @@
 (provide subbag? best-word)
 
 (define (subbag? S B)
+  (cond
+    [(empty? S) #t]
+    [(member (first S) B) (subbag? (rest S) (remove (first S) B))]
+    [else #f])
 )
 
 (define (best-word rack WL)
