f9925eebad7e3e50c3f86f0bc4759d8b8225ae26
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-12 16:55:51 -0800

    Update superduper.rkt

--- hw3/superduper.rkt
+++ hw3/superduper.rkt
@@ -2,15 +2,17 @@
 
 (provide superreverse duperreverse)
 
+;; takes in a list of lists L and returns each top level list reversed
 (define (superreverse L)
   (if (empty? L)
     '()
     (cons (reverse (first L)) (superreverse (rest L)))
 ))
 
+;; takes in a list L and returns each list (top level and every other level) reversed
 (define (duperreverse L)
   (cond
     [(empty? L) '()]
-    [(list? (first L)) (cons (duperreverse (rest L)) (duperreverse (first L)))]
-    [else (cons (duperreverse (rest L)) (first L))]
+    [(list? (first L)) (append (duperreverse (rest L)) (list (duperreverse (first L))))]
+    [else (append (duperreverse (rest L)) (list (first L)))]
 ))
