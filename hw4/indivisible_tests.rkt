766786a6be217a6c3ff42a5b064034208ea5b4f3
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-18 12:46:46 -0800

    Update indivisible_tests.rkt

--- hw4/indivisible_tests.rkt
+++ hw4/indivisible_tests.rkt
@@ -2,3 +2,11 @@
 
 (require rackunit)
 (require "indivisible.rkt")
+
+;; student tests
+(check-equal? (indivisible 1 '(1 2 3 4 5)) '())
+(check-equal? (indivisible 5 '()) '())
+(check-equal? (indivisible 2 '(1 2 3 4 5 6)) '(1 3 5))
+
+;; provided tests
+(check-equal? (indivisible 3 '(2 3 4 5 6 7 8 9 10 )) '(2 4 5 7 8 10))
