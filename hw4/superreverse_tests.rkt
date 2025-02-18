9a115cfd4bd89e4b76b36223141dca4df4158262
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-18 12:36:46 -0800

    Update superreverse_tests.rkt

--- hw4/superreverse_tests.rkt
+++ hw4/superreverse_tests.rkt
@@ -5,7 +5,10 @@
 
 
 ; student tests
-
+(check-equal? (superreverse '())
+                '())
+(check-equal? (superreverse '((cherry banana apple) (mango strawberry kiwi)))
+                  '((apple banana cherry) (kiwi strawberry mango)))
 
 ; provided tests
 (check-equal? (superreverse '( (1 2 3) (4 5 6 (7 8) 9 ) ))
