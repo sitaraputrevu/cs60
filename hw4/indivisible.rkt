8a991bc9db35d17eb61681b4b67984f03800bb86
sitaraputrevu <sputrevu5638@scrippscollege.edu>
2025-02-18 12:44:20 -0800

    Update indivisible.rkt

--- hw4/indivisible.rkt
+++ hw4/indivisible.rkt
@@ -2,6 +2,7 @@
 
 (provide indivisible)
 
-;; add comment
+;; takes in a positive integer e and returns the elements of the list L that are not divisible by E
 (define (indivisible e L)
+  (filter (lambda (num) (not (= (modulo num e) 0))) L)
 )
